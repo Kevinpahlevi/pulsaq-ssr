fa08fea056ecc4a6597ceb16bfd4bffb7d8e55b4
Kevin Pahlevi <kevinplevi@gmail.com>
2020-07-22 17:17:58 +0700

    fix white bg bottom

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ function MyApp(props) {
   }, []);
 
   return (
-    <div style={{ background: "#f7f7f7", height: "100%" }}>
+    <div style={{ background: "#f7f7f7" }}>
       <Head>
         <title>My page</title>
         <meta
